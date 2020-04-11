55249feaa45ee8482d0f024384d9dca81ca36eaa
MohnaSoufian <soufianmohna@gmail.com>
2020-04-11 16:32:33 +0200

    Tarea 2. Ampliación Tarea 1 (Botón "por programa" vs Botón "declarativo")

--- app/src/main/java/com/example/myfirstapp/MainActivity.java
+++ app/src/main/java/com/example/myfirstapp/MainActivity.java
@@ -11,24 +11,38 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    public static final String TAG= "prueba";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        Log.d( "prueba",  "estoy en onCreate()" );   // debug
+        Log.d( TAG,  "estoy en onCreate()" );   // debug
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        // button en programa
         Button wordSizeBtn = (Button) findViewById(R.id.wordSizeBtn);
         wordSizeBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View view){
+                Log.d( TAG,  "onClick function");
                 EditText editText = (EditText) findViewById(R.id.editText);
-                Log.d( "prueba",  editText.getText().toString() );
+                Log.d( TAG,  editText.getText().toString());
                 TextView textView= (TextView) findViewById(R.id.textView);
 
                 int result= editText.getText().toString().length();
-                Log.d("prueba",  editText.getText().toString().length()+"" );
+                Log.d(TAG,  editText.getText().toString().length()+"" );
                 textView.setText(result+"");
             }
         });
+    }
+    // button declarativo
+    public void clickOnSize2Btn(View view) {
+        Log.d( TAG,  "clickOnSize2Btn function");
+        EditText editText = (EditText) findViewById(R.id.editText);
+        Log.d( TAG,  editText.getText().toString() );
+        TextView textView= (TextView) findViewById(R.id.textView);
+
+        int result= editText.getText().toString().length();
+        Log.d(TAG ,  editText.getText().toString().length()+"" );
+        textView.setText(result+"");
     }
 }
